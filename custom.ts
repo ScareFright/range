cbfc7e256d39db0c6cf5c65b26ec7c07d53d5fb7
ScareFright <93850463+ScareFright@users.noreply.github.com>
2021-11-08 09:14:55 -0600

    Update pxt.json, main.ts, custom.ts

--- custom.ts
+++ custom.ts
@@ -1,20 +1,9 @@
 
-/**
-* Use this file to define custom functions and blocks.
-* Read more at https://makecode.microbit.org/blocks/custom
-*/
-
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
 
 /**
  * Custom blocks
  */
-//% weight=100 color=#0fbc11 icon="R"
+//% weight=100color=#bc6e0ficon="R"
 namespace RangeValue {
     /**
      * Find the range of an analog item
@@ -29,13 +18,5 @@ namespace RangeValue {
    basic.showNumber(value)
         value = pins.analogReadPin(AnalogPin.P0)
     }
-    }
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
     }
 }
--- main.ts
+++ main.ts
@@ -1,1 +1,1 @@
-RangeValue.Range(AnalogPin.P0)
+
